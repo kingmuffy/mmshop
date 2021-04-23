166d699873fa5d6f3fbd05e11f27db4eb38447a6
mofe <kingmuffyatse@gmail.com>
2021-04-24 02:44:52 +0400

    model

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import TypeWriter from "react-typewriter";
 import {Row, Col} from 'react-bootstrap'
 import axios from "axios"
 import Product from "../Product"
@@ -20,11 +21,14 @@ const HomeScreen = () => {
     return (
         <>
         <h3>Latest sales</h3>
+        <TypeWriter typing={0.5}><h1>welcome to m shop. definition of saving for quality</h1></TypeWriter>
+
 
         
    <Row> 
        { products.map((product)=>(
             <Col md={6} sm={12} lg={4} xl={3}  >
+
            <Product mofe={product}/>
 
 
